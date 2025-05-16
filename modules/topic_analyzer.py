9af04b81572a293d5bb58017592771dd9af5966a
sudheerakotha <sudheerakotha@gmail.com>
2025-05-16 21:17:12 +0530

    Delete modules directory

--- modules/topic_analyzer.py
+++
@@ -1,6 +0,0 @@
-def extract_topics_from_syllabus(syllabus_text):
-    lines = syllabus_text.split("\n")
-    topics = [line.strip() for line in lines if len(line.strip()) > 3]
-    # Simple prioritization: sort by keyword weight
-    priorities = sorted(topics, key=lambda x: x.count("*") + x.count("important"), reverse=True)
-    return priorities if priorities else topics
